563d7f917b0a15e58dc91ed56d1fa1359646b119
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2021-01-27 20:56:17 +0100

    addming readme

--- main/consumer.py
+++ main/consumer.py
@@ -1,6 +1,6 @@
-import pika
+import pika, json
 import os
-
+from main import Product, db
 
 rabbit_mq = os.environ.get('AMQ_URL')
 params = pika.URLParameters(rabbit_mq)
@@ -13,9 +13,29 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+    if properties.content_type == 'product created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product created')
+
+    elif properties.content_type == 'product updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('product updated')
+
+    elif properties.content_type == 'product deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('product deleted')
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 
 print('Started Consuming')
 
--- main/main.py
+++ main/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_cors import CORS
 from sqlalchemy import UniqueConstraint 
-
+from dataclasses import dataclass
+import requests
+from producer import publish
 
 app = Flask(__name__)
 sql_user=os.environ.get('SQL_USER')
@@ -14,22 +16,49 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
+@dataclass
 class Product(db.Model):
+    id: int
+    title: str
+    image: str
     id = db.Column(db.Integer, primary_key=True, autoincrement=False) # doesnot create but gets form django app
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
-
+@dataclass
 class ProductUser(db.Model):
+    id: int
+    user_id: int
+    product_id: int
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     product_id = db.Column(db.Integer)
 
     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
-@app.route('/')
+@app.route('/api/products')
 def index():
-    return 'Hello'
+    return jsonify(Product.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://docker.for.win.localhost:8000/api/user')
+    json = req.json()
+    print(json)
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+        #event
+
+        publish('product liked', id)
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message' :'success'
+    })
+
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0') 
